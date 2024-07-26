21571a85d524ff4937797da36615efe76e968465
nomadriding <fundora.a@gmail.com>
2024-07-26 18:06:20 -0400

    prep foodlist for AddForm

---
+++ src/components/FoodList.jsx
@@ -0,0 +1,21 @@
+import {useState} from 'react';
+import foodJson from "../foods.json";
+import AddFoodForm from "./AddFoodForm";
+import FoodBox from "./FoodBox";
+
+function FoodList() {
+    const [foods, setFoods] = useState(foodJson);
+
+
+    const handleAddFood = (newFood) => {
+        setFoods([...foods, newFood]);
+    }
+
+  return (
+    <div>FoodList
+        <AddFoodForm onAddFood={handleAddFood} />
+    </div>
+  )
+}
+
+export default FoodList
