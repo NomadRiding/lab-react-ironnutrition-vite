4a64274847c642fb66146bf974a4c292294f80c1
nomadriding <fundora.a@gmail.com>
2024-07-26 19:45:59 -0400

    add css

--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,30 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import './AddFoodForm.css';
 
-function AddFoodForm() {
+function AddFoodForm(props) {
+    const {onAddFood} = props;
     const [name, setName] = useState("");
     const [image, setImage] = useState("");
     const [calories, setCalories] = useState();
+    const [servings, setServings] = useState();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const newFood = {name, image, calories, servings};
+        onAddFood(newFood);
+        setName('');
+        setImage('');
+        setCalories('');
+        setServings('');
+    }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
         <fieldset>
-            <label htmlFor="name">Name: </label>
+            <label htmlFor="name"><strong>Name:</strong> </label>
             <input type="text"
             id="name"
             name="name"
+            placeholder='Food name'
             value={name}
             onChange={(e) => setName(e.target.value)} />            
         </fieldset>
         <fieldset>
-            <label htmlFor="image">Picture: </label>
+            <label htmlFor="image"><strong>Picture:</strong> </label>
             <input type="text"
             id="image"
             name="image"
+            placeholder='Paste URL here'
             value={image}
             onChange={(e) => setImage(e.target.value)} />            
         </fieldset>
         <fieldset>
-            <label htmlFor="calories">Calories: </label>
+            <label htmlFor="calories"><strong>Calories:</strong> </label>
             <input type="number"
             id="calories"
             name="calories"
@@ -33,7 +48,7 @@ function AddFoodForm() {
             onChange={(e) => setCalories(e.target.value)} />            
         </fieldset>
         <fieldset>
-            <label htmlFor="servings">Calories: </label>
+            <label htmlFor="servings"><strong>Servings:</strong> </label>
             <input type="number"
             id="servings"
             name="servings"
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import "./FoodBox.css";
 
-function FoodBox() {
+function FoodBox(props) {
+    const {food, onDelete} = props;
   return (
-    <div>FoodBox</div>
+    <li className="food-list-box">
+        <h3>{food.name}</h3>
+        <img src={food.image} alt="food image" className='food-image' />
+        <p>Calories: {food.calories}</p>
+        <p>Servings: {food.servings}</p>
+        <p className='total-cals'><strong>Total Calories:</strong> {food.servings * food.calories} kcal</p>
+        <button onClick={onDelete}>Delete</button>
+    </li>
   )
 }
 
